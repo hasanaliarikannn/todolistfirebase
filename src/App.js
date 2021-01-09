1339845e34a0fe8f38b92295d6b9f52a72748569
hasanaliarikannn <hasanaliarikan077@hotmail.com>
2021-01-09 17:05:57 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import database from "./firebase";
+import firebase from "firebase";
 
-function App() {
+import "./App.css";
+
+const App = () => {
+  const [input, Setinput] = useState("");
+  const [values, Setvalues] = useState([]);
+
+  useEffect(() => {
+    database
+      .collection("todo")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        Setvalues(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        )
+      );
+  }, []);
+  function deletedata (value){
+    database.collection("todo").doc(value).delete()
+  }
+
+  function kaydetdatabase(value) {
+    database.collection("todo").add({
+      messages: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    Setinput("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <input
+          type="text"
+          placeholder="veri"
+          value={input}
+          onChange={(e) => Setinput(e.target.value)}
+        />
+        <button disabled={!input} onClick={() => kaydetdatabase(input)}>
+          Veri Kaydet
+        </button>
       </header>
+      <div className="messages">
+        {values.map((item) => (
+          <ul>
+            <li>
+              {item.data.messages}{" "}
+              <div>
+                <button onClick={()=>deletedata(item.id)}>Delete</button>
+                
+              </div>
+            </li>
+          </ul>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
